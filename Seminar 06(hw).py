0c9b88b22de76821065965b5b225e81bbda920bd
Sergey_Tyutvin <tyutvi@gmail.com>
2022-09-21 20:27:49 +0300

    Задача №1 решена

---
+++ Seminar 06(hw).py
@@ -0,0 +1,16 @@
+# Задача №1. Сгенерируйте случайный список из чисел.
+# Увеличьте значение каждого элемента на 10 и запишите в новый список.
+
+# def rand_list(n):
+#     from random import randint
+#     some_list=[]
+#     for i in range(n):
+#         some_list.append(randint(1,10))
+#     return some_list
+
+# x=int(input('Введите количество элемнтов массива: '))
+# s1=rand_list(x)
+# print(s1)
+# s2=list(map(lambda y: y+10, s1))
+# print(s2)
+
